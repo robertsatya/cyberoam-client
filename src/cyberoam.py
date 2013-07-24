465e5a7e801026deb429f4e449ea3799dd645fbc
Harsha Satya <robertsatya@gmail.com>
2013-07-25 00:36:24 +0530

    license added

--- src/cyberoam.py
+++ src/cyberoam.py
@@ -5,7 +5,13 @@
 # The Client uses data extraction method mentioned in Linc (Linc Is Not Cyberoam) Open Source Project
 # Credit to Siddhartha Sahu for the valuable inputs
 # Documentation to be uploaded soon
-
+# License:  This  program  is  free  software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published by
+# the  Free Software Foundation; either version 3 of the License, or (at your
+# option)  any later version. This program is distributed in the hope that it
+# will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+# Public License for more details.
 
 from PyQt4 import QtGui, QtCore
 from xml.dom.minidom import parseString
